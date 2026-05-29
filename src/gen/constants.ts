df6e257063ccca516e7a3b923a6126f4646363cb
illya-klus <illyaklus2@gmail.com>
2026-05-30 00:07:02 +0300

    Update: add constants

---
+++ src/gen/constants.ts
@@ -0,0 +1,9 @@
+export const AUTH_PACKAGE = "auth.v1";
+export const AUTH_PROTO_PATH = "node_modules/@kinvue/contracts/proto/auth.proto"
+export const AUTH_SERVICE_NAME = "AuthService";
+
+
+export const USER_PACKAGE = "user.v1";
+export const USER_PROTO_PATH = "node_modules/@kinvue/contracts/proto/user.proto";
+export const USER_SERVICE_NAME = "UserService";
+
